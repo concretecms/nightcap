09bffb3f124a4c15529b1324f3f44294730f9fdb
Andrew Embler <andrew@concrete5.org>
2019-03-15 14:01:12 -0700

    simplified logging using the new logging middleware

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -1,14 +1,13 @@
 <?php
 namespace Concrete\Nightcap;
 
+use Concat\Http\Middleware\Logger;
 use Concrete\Nightcap\OAuth2\Middleware\MiddlewareFactory;
-use Concrete\Nightcap\Provider\ProviderInterface;
 use Concrete\Nightcap\Service\ServiceCollection;
 use Concrete\Nightcap\Service\ServiceDescriptionFactory;
 use GuzzleHttp\HandlerStack;
-use GuzzleHttp\MessageFormatter;
-use GuzzleHttp\Middleware;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 
 class ClientFactory implements ClientFactoryInterface
 {
@@ -62,31 +61,15 @@ class ClientFactory implements ClientFactoryInterface
         return $this->middlewareFactory;
     }
 
-
-    private function createLoggingMiddleware(string $messageFormat)
-    {
-        return Middleware::log($this->logger, new MessageFormatter($messageFormat));
-    }
-
-    private function addLoggingHandler(HandlerStack $stack, array $messageFormats)
-    {
-        foreach($messageFormats as $messageFormat) {
-            $stack->unshift(
-                $this->createLoggingMiddleware($messageFormat)
-            );
-        }
-        return $stack;
-    }
-
     public function create($config = [])
     {
         $oauth = $this->middlewareFactory->create();
         $stack = HandlerStack::create();
         if ($this->logger) {
-            $stack = $this->addLoggingHandler($stack, [
-                '{method} {uri} HTTP/{version} {req_body}',
-                'RESPONSE: {code} - {res_body}',
-            ]);
+            $middleware = new Logger($this->logger);
+            $middleware->setRequestLoggingEnabled(true);
+            $middleware->setLogLevel(LogLevel::INFO);
+            $stack->unshift($middleware);
         }
 
         $stack->push($oauth);
